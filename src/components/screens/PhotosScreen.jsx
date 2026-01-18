349527e0668b01dd82627af4a2ac7ea11a9004dd
demouse890-source <demouse890@gmail.com>
2026-01-18 22:41:30 +0530

    Update PhotosScreen.jsx

--- src/components/screens/PhotosScreen.jsx
+++ src/components/screens/PhotosScreen.jsx
@@ -13,10 +13,11 @@ export default function PhotosScreen({ onNext }) {
   const swiperRef = useRef(null)
 
   const photos = [
-    "/images/1.jpeg",
-    "/images/2.jpeg",
-    "/images/3.jpeg",
-    "/images/4.jpeg",
+    "/images/(1).jpeg",
+    "/images/(2).jpeg",
+    "/images/(3).jpeg",
+    "/images/(4).jpeg",
+    "/images/(5).jpeg",
   ]
 
   return (
